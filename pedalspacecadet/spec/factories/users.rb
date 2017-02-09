f8f5d0981b1f5497bfd89c897cdf757e2c7d9b35
Tana Jukes <tana.jukes@gmail.com>
2017-02-09 15:11:51 -0800

    User last name: use Faker in factory

--- pedalspacecadet/spec/factories/users.rb
+++ pedalspacecadet/spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     type ""
     first_name Faker::Name.first_name
-    last_name "MyString"
+    last_name Faker::Name.last_name
     photo "MyString"
     email "MyString"
     password_digest "MyString"
